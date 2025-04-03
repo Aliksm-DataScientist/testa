3af27cdef15b8ff4a4e3e182c3bb9ce6d9c0e036
Aliksm Shaik <ashaik.dev123@gmail.com>
2025-04-03 18:50:19 +0530

    updated version in docker img

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
 
     
-    return f"<h1>Hello CI / CD Version</h1>"
+    return f"<h1>Hello CI / CD 111111</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
